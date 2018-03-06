2b829e4588583e300250433a696f7b6c33eb408b
Derek Witteck <drwitteck@gmail.com>
2018-03-06 00:08:56 -0500

    Minor edits

--- app/src/main/java/com/drwitteck/mymappappdemo/MainActivity.java
+++ app/src/main/java/com/drwitteck/mymappappdemo/MainActivity.java
@@ -6,10 +6,12 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.PersistableBundle;
-import android.support.v4.app.ActivityCompat;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -22,57 +24,75 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    MapView mapView;
-    GoogleMap googleMap;
     LocationManager lm;
     LocationListener ll;
-
+    Location currentLocation;
+    TextView lat;
+    TextView longitude;
+    MapView mapView;
+    GoogleMap googleMap;
     LatLng latLng;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mapView = findViewById(R.id.mapView);
         lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        /**
+         * in each lifecycle call back must have mapView.CallBack(); that corresponds
+         */
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        findViewById(R.id.dropPinButton).setOnClickListener(v -> {
+            MarkerOptions marker = new MarkerOptions().position(latLng).title("My Location");
+            googleMap.addMarker(marker);
+        });
+
+        lat = findViewById(R.id.latText);
+        longitude = findViewById(R.id.longitudeText);
 
         ll = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                //Camera update to go to this location
-                CameraUpdate cu = CameraUpdateFactory.newLatLng(latLng);
+                latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                CameraUpdate cu = CameraUpdateFactory.newLatLngZoom(latLng,16);
                 googleMap.animateCamera(cu);
+
+                currentLocation = location;
+                lat.setText(location.getLatitude()+"");
+                longitude.setText(location.getLongitude()+"");
             }
 
             @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-
-            }
-
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
             @Override
-            public void onProviderEnabled(String s) {
-
-            }
-
+            public void onProviderEnabled(String provider) {}
             @Override
-            public void onProviderDisabled(String s) {
-
-            }
+            public void onProviderDisabled(String provider) {}
         };
+    }
 
-        mapView = findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
+    @SuppressWarnings("MissingPermission")
+    private void requestUpdates(){
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0,ll);
+        lm.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER,0,0,ll);
+    }
 
-        mapView.getMapAsync(this);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        findViewById(R.id.button).setOnClickListener((View v) -> {
-            MarkerOptions marker = new MarkerOptions().position(latLng);
-
-            googleMap.addMarker(marker);
-        });
-
+        if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            requestUpdates();
+        }else{
+            Toast.makeText(this, "Not granted.", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 
     @Override
@@ -82,29 +102,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume(){
         super.onResume();
         mapView.onResume();
-
-        if (ActivityCompat.checkSelfPermission
-                (this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED) {
-            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
-            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, ll);
-        } else {
-            onRequestPermissionsResult(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION});
+        if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION ) ==
+                PackageManager.PERMISSION_GRANTED){
+            requestUpdates();
+        }else{
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
         }
-    }
 
-    public void onRequestPermissionsResult(String permissions[]) {
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1111);
     }
 
     @Override
-    protected void onPause() {
+    protected void onPause(){
         super.onPause();
         mapView.onPause();
+        lm.removeUpdates(ll);
     }
 
     @Override
@@ -122,10 +136,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 
     @Override
-    public void onMapReady (GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
     }
 }
